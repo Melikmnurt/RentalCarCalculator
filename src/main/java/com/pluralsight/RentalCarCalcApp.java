0fa856edaaf616113dfca648ad022f21e60b51ca
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-04-14 11:00:04 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/RentalCarCalcApp.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+import.java.util.Scanner;
+
+public class RentalCarCal {
+    public static void main(String[] args) {
+
+        //in the date variable
+        String date = theScanner.nextline();
+
+    }
+}
